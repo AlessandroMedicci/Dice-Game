9bc1874c7cacb7455c55c6e3265c79c0f5fd1ada
AlessandroMedicci <alessandro.medicci@gmail.com>
2022-09-14 12:14:16 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Roll The Dice
+
+- The game is simple and funny.
+- Player 1 rolls a dice and collects the scores.
+- There are two ways to lose the round: 1) Player clicks the HOLD button in order to save the scores and the round gets switched to Player 2. 2) Due to the fact that dices are produced randomly - if the dice number 1 appears that means the round has been lost and the game gets switched back to previous player.
+  \*\* NOTE: if a current player gets the dice 1 during rolling and the one had some scores collected during the round - the dice 1 cuts off all the scores that were not HOLD to save.
+- Maximum score is 100 and whoever reaches it the winner is being declared.
+- The game can be restarted by clicking NEW GAME button during the ongoing round.
+
+✔✔✔
+const DECLARATION = `I did not come up with this project idea. Project itself belongs to the greatest mentor ${Jonas Schmedtmann}. The following project was one of the challenge during the learning process and I had to accomplish it by writing the code with all the conditions and logics taken into consideration`
---
+++ script.js
@@ -0,0 +1,97 @@
+'use strict';
+
+//Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
+
+// Switch function
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling buttons functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    // 2. Display a Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check the roll
+    if (dice !== 1) {
+      // Add the result to the current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      //current0El.textContent = currentScore; //Changes latter
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current scores on total score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. If players score === 100 // finis the game
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 3. If !==100 switch the turn
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
